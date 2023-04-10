0b9863adcdea81813ad10977f2d014eaa67ae02d
unknown <jonathanoktavianofrizzy@gmail.com>
2023-04-10 10:20:03 +0700

    revisi soal pendahuluan nomer 1

--- praktikum_2.c
+++ praktikum_2.c
@@ -15,6 +15,318 @@ void p2_tugas_pendahuluan_soal_nomer_1()
     }
 }
 
+void p2_tugas_pendahuluan_soal_nomer_1_2nd()
+{
+    int input_nilai;
+    printf("Masukkan nilai ujian : ");
+    scanf("%d", &input_nilai);
+    switch (input_nilai)
+    {
+    case 1:
+        printf("Anda tidak lulus\n");
+        break;
+    case 2:
+        printf("Anda tidak lulus\n");
+        break;
+    case 3:
+        printf("Anda tidak lulus\n");
+        break;
+    case 4:
+        printf("Anda tidak lulus\n");
+        break;
+    case 5:
+        printf("Anda tidak lulus\n");
+        break;
+    case 6:
+        printf("Anda tidak lulus\n");
+        break;
+    case 7:
+        printf("Anda tidak lulus\n");
+        break;
+    case 8:
+        printf("Anda tidak lulus\n");
+        break;
+    case 9:
+        printf("Anda tidak lulus\n");
+        break;
+    case 10:
+        printf("Anda tidak lulus\n");
+        break;
+    case 11:
+        printf("Anda tidak lulus\n");
+        break;
+    case 12:
+        printf("Anda tidak lulus\n");
+        break;
+    case 13:
+        printf("Anda tidak lulus\n");
+        break;
+    case 14:
+        printf("Anda tidak lulus\n");
+        break;
+    case 15:
+        printf("Anda tidak lulus\n");
+        break;
+    case 16:
+        printf("Anda tidak lulus\n");
+        break;
+    case 17:
+        printf("Anda tidak lulus\n");
+        break;
+    case 18:
+        printf("Anda tidak lulus\n");
+        break;
+    case 19:
+        printf("Anda tidak lulus\n");
+        break;
+    case 20:
+        printf("Anda tidak lulus\n");
+        break;
+    case 21:
+        printf("Anda tidak lulus\n");
+        break;
+    case 22:
+        printf("Anda tidak lulus\n");
+        break;
+    case 23:
+        printf("Anda tidak lulus\n");
+        break;
+    case 24:
+        printf("Anda tidak lulus\n");
+        break;
+    case 25:
+        printf("Anda tidak lulus\n");
+        break;
+    case 26:
+        printf("Anda tidak lulus\n");
+        break;
+    case 27:
+        printf("Anda tidak lulus\n");
+        break;
+    case 28:
+        printf("Anda tidak lulus\n");
+        break;
+    case 29:
+        printf("Anda tidak lulus\n");
+        break;
+    case 30:
+        printf("Anda tidak lulus\n");
+        break;
+    case 31:
+        printf("Anda tidak lulus\n");
+        break;
+    case 32:
+        printf("Anda tidak lulus\n");
+        break;
+    case 33:
+        printf("Anda tidak lulus\n");
+        break;
+    case 34:
+        printf("Anda tidak lulus\n");
+        break;
+    case 35:
+        printf("Anda tidak lulus\n");
+        break;
+    case 36:
+        printf("Anda tidak lulus\n");
+        break;
+    case 37:
+        printf("Anda tidak lulus\n");
+        break;
+    case 38:
+        printf("Anda tidak lulus\n");
+        break;
+    case 39:
+        printf("Anda tidak lulus\n");
+        break;
+    case 40:
+        printf("Anda tidak lulus\n");
+        break;
+    case 41:
+        printf("Anda tidak lulus\n");
+        break;
+    case 42:
+        printf("Anda tidak lulus\n");
+        break;
+    case 43:
+        printf("Anda tidak lulus\n");
+        break;
+    case 44:
+        printf("Anda tidak lulus\n");
+        break;
+    case 45:
+        printf("Anda tidak lulus\n");
+        break;
+    case 46:
+        printf("Anda tidak lulus\n");
+        break;
+    case 47:
+        printf("Anda tidak lulus\n");
+        break;
+    case 48:
+        printf("Anda tidak lulus\n");
+        break;
+    case 49:
+        printf("Anda tidak lulus\n");
+        break;
+    case 50:
+        printf("Anda tidak lulus\n");
+        break;
+    case 51:
+        printf("Anda tidak lulus\n");
+        break;
+    case 52:
+        printf("Anda tidak lulus\n");
+        break;
+    case 53:
+        printf("Anda tidak lulus\n");
+        break;
+    case 54:
+        printf("Anda tidak lulus\n");
+        break;
+    case 55:
+        printf("Anda tidak lulus\n");
+        break;
+    case 56:
+        printf("Anda tidak lulus\n");
+        break;
+    case 57:
+        printf("Anda tidak lulus\n");
+        break;
+    case 58:
+        printf("Anda tidak lulus\n");
+        break;
+    case 59:
+        printf("Anda tidak lulus\n");
+        break;
+    case 60:
+        printf("Anda tidak lulus\n");
+        break;
+    case 61:
+        printf("Anda tidak lulus\n");
+        break;
+    case 62:
+        printf("Anda tidak lulus\n");
+        break;
+    case 63:
+        printf("Anda tidak lulus\n");
+        break;
+    case 64:
+        printf("Anda tidak lulus\n");
+        break;
+    case 65:
+        printf("Anda tidak lulus\n");
+        break;
+    case 66:
+        printf("Anda tidak lulus\n");
+        break;
+    case 67:
+        printf("Anda tidak lulus\n");
+        break;
+    case 68:
+        printf("Anda tidak lulus\n");
+        break;
+    case 69:
+        printf("Anda tidak lulus\n");
+        break;
+    case 70:
+        printf("Anda lulus\n");
+        break;
+    case 71:
+        printf("Anda lulus\n");
+        break;
+    case 72:
+        printf("Anda lulus\n");
+        break;
+    case 73:
+        printf("Anda lulus\n");
+        break;
+    case 74:
+        printf("Anda lulus\n");
+        break;
+    case 75:
+        printf("Anda lulus\n");
+        break;
+    case 76:
+        printf("Anda lulus\n");
+        break;
+    case 77:
+        printf("Anda lulus\n");
+        break;
+    case 78:
+        printf("Anda lulus\n");
+        break;
+    case 79:
+        printf("Anda lulus\n");
+        break;
+    case 80:
+        printf("Anda lulus\n");
+        break;
+    case 81:
+        printf("Anda lulus\n");
+        break;
+    case 82:
+        printf("Anda lulus\n");
+        break;
+    case 83:
+        printf("Anda lulus\n");
+        break;
+    case 84:
+        printf("Anda lulus\n");
+        break;
+    case 85:
+        printf("Anda lulus\n");
+        break;
+    case 86:
+        printf("Anda lulus\n");
+        break;
+    case 87:
+        printf("Anda lulus\n");
+        break;
+    case 88:
+        printf("Anda lulus\n");
+        break;
+    case 89:
+        printf("Anda lulus\n");
+        break;
+    case 90:
+        printf("Anda lulus\n");
+        break;
+    case 91:
+        printf("Anda lulus\n");
+        break;
+    case 92:
+        printf("Anda lulus\n");
+        break;
+    case 93:
+        printf("Anda lulus\n");
+        break;
+    case 94:
+        printf("Anda lulus\n");
+        break;
+    case 95:
+        printf("Anda lulus\n");
+        break;
+    case 96:
+        printf("Anda lulus\n");
+        break;
+    case 97:
+        printf("Anda lulus\n");
+        break;
+    case 98:
+        printf("Anda lulus\n");
+        break;
+    case 99:
+        printf("Anda lulus\n");
+        break;
+    case 100:
+        printf("Anda lulus\n");
+        break;
+    default:
+        break;
+    }
+}
+
 void p2_tugas_pendahuluan_soal_nomer_2()
 {
     int input_nomer_hari;
